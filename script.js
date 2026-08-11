a5e43882e8402e250e6e828cf5b71e049f9decf3
Fazminaz <85796518+Fazminaz@users.noreply.github.com>
2026-08-11 13:53:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let window = new Object();
+
+    window.menu = document.createElement("div");
+    window.menu.id = "menu-container";
+    window.menu.classList.add("window");
+
+    window.menu.backgroundTransition = document.createElement("div");
+    window.menu.backgroundTransition.classList.add("window-background-transition");
+    window.menu.backgroundTransition.appendChild(document.createElement("div"));
+    window.menu.appendChild(window.menu.backgroundTransition);
+
+    window.menu.backgroundGradient = document.createElement("div");
+    window.menu.backgroundGradient.classList.add("window-background-gradient");
+    window.menu.appendChild(window.menu.backgroundGradient);
+
+    window.menu.content = document.createElement("div");
+    window.menu.content.classList.add("window-content");
+    window.menu.appendChild(window.menu.content);
+
+    window.menu.content.logo = document.createElement("h1");
+    window.menu.content.logo.id = "menu-logo";
+    window.menu.content.logo.innerHTML = "<div>Simple</div><div>Maths</div>";
+    window.menu.content.appendChild(window.menu.content.logo);
+
+    window.menu.content.buttonPlay = document.createElement("button");
+    window.menu.content.buttonPlay.id = "menu-button-play";
+    window.menu.content.buttonPlay.innerHTML = "<span>Play</span>";
+    window.menu.content.appendChild(window.menu.content.buttonPlay);
+
+    document.body.appendChild(window.menu);
+})
