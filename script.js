2934cb8e48da7fc5f1555a0bb10124670f89c441
Azarothix <85796518+AzathothG@users.noreply.github.com>
2026-08-12 10:22:06 +0000

    Untested changes in all files

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
     let window = new Object();
 
-    window.menu = document.createElement("div");
-    window.menu.id = "menu-container";
-    window.menu.classList.add("window", "window-fade-in");
+    function windowInitalize(windowIdentifier, windowContentInitalize) {
+        let window = window[windowIdentifier];
+
+        window[windowIdentifier] = document.createElement("div");
+        window[windowIdentifier].id = windowIdentifier + "-container";
+        window[windowIdentifier].classList.add("window");
+
+        window[windowIdentifier].backgroundTransition = document.createElement("div");
+        window[windowIdentifier].backgroundTransition.classList.add("window-background-transition");
+        window[windowIdentifier].backgroundTransition.appendChild(document.createElement("div"));
+        window[windowIdentifier].appendChild(window[windowIdentifier].backgroundTransition);
+
+        window[windowIdentifier].backgroundGradient = document.createElement("div");
+        window[windowIdentifier].backgroundGradient.classList.add("window-background-gradient");
+        window[windowIdentifier].appendChild(window[windowIdentifier].backgroundGradient);
+
+        window[windowIdentifier].content = document.createElement("div");
+        window[windowIdentifier].content.classList.add("window-content");
+
+        windowContentInitalize(window[windowIdentifier].content);
+
+        window[windowIdentifier].appendChild(window[windowIdentifier].content);
+
+        return undefined;
+    }
+
+    windowInitalize("menu", (content) => {
+        content.logo = document.createElement("h1");
+        content.logo.id = "menu-logo";
+        content.logo.innerHTML = "<div>Simple</div><div>Maths</div>";
+        content.appendChild(content.logo);
+
+        content.buttonPlay = document.createElement("button");
+        content.buttonPlay.id = "menu-button-play";
+        content.buttonPlay.innerHTML = "<span>Play</span>";
+        content.buttonPlay.addEventListener("click", () => {
+            window.menu.classList.add("window-fade-out");
+            setInterval(() => {
+                window.menu.remove();
+                window.menu.classList.remove("window-fade-out");
+
+                window.play.classList.add("window-fade-in");
+                document.body.appendChild(window.play);
+            }, 1000);
+
+            setInterval(() => {
+                window.play.classList.remove("window-fade-in");
+            }, 2000);
+        });
+
+        window.menu.content.appendChild(window.menu.content.buttonPlay);
+    });
+    windowInitalize("play");
+
+    document.body.appendChild(window.menu);
+
     setInterval(() => {
         window.menu.classList.remove("window-fade-in");
     }, 1000);
-
-    window.menu.backgroundTransition = document.createElement("div");
-    window.menu.backgroundTransition.classList.add("window-background-transition");
-    window.menu.backgroundTransition.appendChild(document.createElement("div"));
-    window.menu.appendChild(window.menu.backgroundTransition);
-
-    window.menu.backgroundGradient = document.createElement("div");
-    window.menu.backgroundGradient.classList.add("window-background-gradient");
-    window.menu.appendChild(window.menu.backgroundGradient);
-
-    window.menu.content = document.createElement("div");
-    window.menu.content.classList.add("window-content");
-    window.menu.appendChild(window.menu.content);
-
-    window.menu.content.logo = document.createElement("h1");
-    window.menu.content.logo.id = "menu-logo";
-    window.menu.content.logo.innerHTML = "<div>Simple</div><div>Maths</div>";
-    window.menu.content.appendChild(window.menu.content.logo);
-
-    window.menu.content.buttonPlay = document.createElement("button");
-    window.menu.content.buttonPlay.id = "menu-button-play";
-    window.menu.content.buttonPlay.innerHTML = "<span>Play</span>";
-    window.menu.content.buttonPlay.addEventListener("click", () => {
-        window.menu.classList.add("window-fade-out");
-        setInterval(() => {
-            window.menu.remove();
-            window.menu.classList.remove("window-fade-out");
-        }, 1000);
-    });
-    window.menu.content.appendChild(window.menu.content.buttonPlay);
-
-    document.body.appendChild(window.menu);
 })
