980166db34765a1cf8db3171ee15a3625dd4a9f2
Azarothix <85796518+AzathothG@users.noreply.github.com>
2026-08-12 21:57:21 +0530

    Removed experimental code

--- script.js
+++ script.js
@@ -66,20 +66,4 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
         window.menu.classList.remove("window-fade-in");
     }, 1000);
-
-    document.body.appendChild(document.createElement("img"));
-    document.querySelector("img").src = "./assets/rupture.png";
-    document.querySelector("img").style.position = "fixed";
-    document.querySelector("img").style.scale = "1";
-    document.querySelector("img").style.width = "400px";
-    document.querySelector("img").style.height = "400px";
-
-    setInterval(() => {
-        document.querySelector("img").style.scale = "1";
-        document.querySelector("img").style.transform = "translate(" + String(Math.floor(Math.random() * window.innerWidth) - Math.floor(document.querySelector("img").width / 2)) + "px, " + String(Math.floor(Math.random() * window.innerHeight) - Math.floor(document.querySelector("img").height / 2)) + "px)";
-    }, 5000);
-
-    setInterval(() => {
-        document.querySelector("img").style.scale = String(Number(document.querySelector("img").style.scale) - 0.01);
-    }, 50);
 })
